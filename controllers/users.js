5c0c238378a6099899b67d00c6035bdb58e824e1
Josué Lobo <darksiders20005@gmail.com>
2022-02-04 20:52:53 -0300

    feat: implementado regra de negocio de email

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,20 @@
 const rescue = require('express-rescue');
 const router = require('express').Router();
+const {
+  validateName,
+  validateEmail,
+  validatePassword,
+} = require('./middlewares/validations');
+const userService = require('../services/user');
 
 router.post('/',
+  validateName,
+  validateEmail,
+  validatePassword,
   rescue(async (req, res) => {
-    const token = req.headers.authorization;
-    res.status(201).json(token)
+    const user = req.body;
+    const newUser = await userService.create(user);
+    res.status(201).json(newUser);
   }));
 
 module.exports = router;
---
+++ services/user.js
@@ -0,0 +1,26 @@
+const jwt = require('../utils/jwt');
+const { User } = require('../models');
+
+const emailValid = async (user) => {
+  const { email } = user;
+  const findEmail = await User.findOne({ where: {email} })
+  if (findEmail) {
+    const error = new Error();
+    error.code = 409;
+    error.message = 'User already registered';
+    throw error;
+  }
+}
+
+const create = async (user) => {
+  const { displayName } = user;
+  await emailValid(user);
+  await User.create(user);
+
+  const token = jwt.sign({ displayName });
+
+  console.log(token)
+  return token;
+};
+
+module.exports = { create };
