077e3e73110225db4f4cd7b06bd8edd33a5b1a62
Josué Lobo <darksiders20005@gmail.com>
2022-02-04 19:14:47 -0300

    ci: implementado o jwt

---
+++ utils/jwt.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+
+const sign = (payload, duration = '1H') => {
+  jwt.sign(payload, JWT_SECRET, {
+    algorithm: 'HS256',
+    expiresIn: duration,
+  });
+};
+
+const verify = (token) => jwt.verify(
+  token, JWT_SECRET, { algorithms: ['HS256'] },
+);
+
+module.exports = { sign, verify };
