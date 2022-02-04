8a478889ba39b2c08426bd38dc0aab50c119a59a
Josué Lobo <darksiders20005@gmail.com>
2022-02-04 17:34:50 -0300

    feat: adicionando validações de input de name, email e password

---
+++ controllers/middlewares/validations.js
@@ -0,0 +1,43 @@
+const emailRegex = /\S+@\S+\.\S+/;
+
+const validateName = (req, res, next) => {
+  const { displayName } = req.body;
+
+  if (!displayName || displayName.length < 8) {
+    return res.status(400).json({
+      message: '"displayName" length must be at least 8 characters long',
+    });
+  }
+
+  next();
+};
+
+const validateEmail = async (req, res, next) => {
+  const { email } = req.body;
+
+  if (!email) return res.status(400).json({ message: '"email" is required' });
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: '"email" must be a valid email' });
+  }
+
+  next();
+};
+
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+
+  if (!password) return res.status(400).json({ message: '"password" is required' });
+  if (password.length < 6) {
+    return res.status(400).json(
+      { message: '"password" length must be 6 characters long' },
+    );
+  }
+
+  next();
+};
+
+module.exports = {
+  validateName,
+  validateEmail,
+  validatePassword,
+}; 
