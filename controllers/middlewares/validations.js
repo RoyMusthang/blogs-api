edcd20c8ea13765e4dec92af7272cf0bc8edde95
Josué Lobo <darksiders20005@gmail.com>
2022-02-04 22:58:16 -0300

    feat: validações implementadas

--- controllers/middlewares/validations.js
+++ controllers/middlewares/validations.js
@@ -36,8 +36,27 @@ const validatePassword = (req, res, next) => {
   next();
 };
 
+const validateLogin = (req, res, next) => {
+const { email, password } = req.body;
+
+  if (password === '') {
+    return res.status(400).json({ message: '"password" is not allowed to be empty' });
+  }
+  if (!password) return res.status(400).json(
+    { message: '"password" is required' }
+  );
+  if (email === '') {
+    return res.status(400).json({ message: '"email" is not allowed to be empty' });
+  }
+  if (!email) return res.status(400).json(
+    { message: '"email" is required' }
+  );
+  next();
+}
+
 module.exports = {
   validateName,
   validateEmail,
   validatePassword,
+  validateLogin,
 }; 
