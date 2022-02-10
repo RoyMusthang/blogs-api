1f092b942219676762f3cbce78b5fd46345ab320
Josué Lobo <darksiders20005@gmail.com>
2022-02-10 01:00:59 -0300

    fix: mudando validação de lugar estourar o erro no lugar certo

--- services/login.js
+++ services/login.js
@@ -5,14 +5,14 @@ const login = async (data) => {
   const { email, password } = data;
 
   const user = await User.findOne({ where: { email } });
-  const { id } = user;
-  const token = jwt.sign({ email, id });
   if (!user || password !== user.password) {
     const error = new Error();
     error.code = 400;
     error.message = 'Invalid fields';
     throw error;
   }
+  const { id } = user;
+  const token = jwt.sign({ email, id });
   return token;
 };
 
